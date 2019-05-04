837676ca8fd007fff67293e080da3e6d29ed333c
Robocop79 <33665206+Robocop79@users.noreply.github.com>
2019-05-03 22:40:07 -0500

    Create vbackplay404.js

---
+++ JS/vbackplay404.js
@@ -0,0 +1,14 @@
+new BackgroundVideo({
+    video: [
+        {
+            file: "https://robocop79.github.io/video/PS2SS.mp4"
+        },
+        {
+            file: "https://robocop79.github.io/video/PS2SSloop.mp4",
+        },
+        {
+            file: "https://robocop79.github.io/video/PS2SSloop.mp4"
+        }
+    ],
+    mobileImg: "https://robocop79.github.io/images/Subscribe.png"
+});
