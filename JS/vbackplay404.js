9dfa683af253ccabd2fd0442416dbdeed341532b
Robocop79 <33665206+Robocop79@users.noreply.github.com>
2019-05-05 15:12:40 -0500

    Now works with 5 times loop

--- JS/vbackplay404.js
+++ JS/vbackplay404.js
@@ -1,8 +1,11 @@
 new BackgroundVideo({
     video: [
         {file: "https://robocop79.github.io/video/PS2SS.mp4"},
-        {file: "https://robocop79.github.io/video/PS2SSloop.mp4"},
-        {file: "https://robocop79.github.io/video/PS2SSloop.mp4"}
+        {file: "https://robocop79.github.io/video/ps2rsodloop.mp4"},
+        {file: "https://robocop79.github.io/video/ps2rsodloop.mp4"},
+        {file: "https://robocop79.github.io/video/ps2rsodloop.mp4"},
+        {file: "https://robocop79.github.io/video/ps2rsodloop.mp4"},
+        {file: "https://robocop79.github.io/video/ps2rsodloop.mp4"}
     ],
     mobileImg: "https://robocop79.github.io/images/Subscribe.png"
 });
