9c94cb6d5187799da94aca05c3b15e60c8f91a4a
Robocop79 <33665206+Robocop79@users.noreply.github.com>
2019-05-05 14:30:33 -0500

    update code

--- JS/vbackplay404.js
+++ JS/vbackplay404.js
@@ -1,14 +1,8 @@
 new BackgroundVideo({
     video: [
-        {
-            file: "https://robocop79.github.io/video/PS2SS.mp4"
-        },
-        {
-            file: "https://robocop79.github.io/video/PS2SSloop.mp4",
-        },
-        {
-            file: "https://robocop79.github.io/video/PS2SSloop.mp4"
-        }
+        {file: "https://robocop79.github.io/video/PS2SS.mp4"},
+        {file: "https://robocop79.github.io/video/PS2SSloop.mp4"},
+        {file: "https://robocop79.github.io/video/PS2SSloop.mp4"}
     ],
     mobileImg: "https://robocop79.github.io/images/Subscribe.png"
 });
