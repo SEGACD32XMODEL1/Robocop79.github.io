c179787b34c71686eac26e0376d681b2cc1d0e89
Robocop79 <33665206+Robocop79@users.noreply.github.com>
2019-05-05 15:40:58 -0500

    fixed loop sync and added rsod intro

--- JS/vbackplay404.js
+++ JS/vbackplay404.js
@@ -1,6 +1,6 @@
 new BackgroundVideo({
     video: [
-        {file: "https://robocop79.github.io/video/PS2SS.mp4"},
+        {file: "https://robocop79.github.io/video/ps2rsodtrim.mp4"},
         {file: "https://robocop79.github.io/video/ps2rsodloop.mp4"},
         {file: "https://robocop79.github.io/video/ps2rsodloop.mp4"},
         {file: "https://robocop79.github.io/video/ps2rsodloop.mp4"},
