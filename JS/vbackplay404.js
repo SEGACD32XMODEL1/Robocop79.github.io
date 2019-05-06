6067c0ae19c36539a1311bff356ce584f7248201
Robocop79 <33665206+Robocop79@users.noreply.github.com>
2019-05-06 11:55:12 -0500

    smaller video files

--- JS/vbackplay404.js
+++ JS/vbackplay404.js
@@ -1,11 +1,11 @@
 new BackgroundVideo({
     video: [
-        {file: "https://robocop79.github.io/video/9min25rsod.mp4"},
-        {file: "https://robocop79.github.io/video/rsodloopgood.mp4"},
-        {file: "https://robocop79.github.io/video/rsodloopgood.mp4"},
-        {file: "https://robocop79.github.io/video/rsodloopgood.mp4"},
-        {file: "https://robocop79.github.io/video/rsodloopgood.mp4"},
-        {file: "https://robocop79.github.io/video/rsodloopgood.mp4"}
+        {file: "https://robocop79.github.io/video/5minrsod.mp4"},
+        {file: "https://robocop79.github.io/video/rsodloopbetter.mp4"},
+        {file: "https://robocop79.github.io/video/rsodloopbetter.mp4"},
+        {file: "https://robocop79.github.io/video/rsodloopbetter.mp4"},
+        {file: "https://robocop79.github.io/video/rsodloopbetter.mp4"},
+        {file: "https://robocop79.github.io/video/rsodloopbetter.mp4"}
     ],
     mobileImg: "https://robocop79.github.io/images/Subscribe.png"
     
