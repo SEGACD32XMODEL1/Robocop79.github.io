6cc1de7947c6e89face44f0c7f8460f5aa857b20
Robocop79 <33665206+Robocop79@users.noreply.github.com>
2019-05-06 12:27:03 -0500

    Update vbackplay404.js

--- JS/vbackplay404.js
+++ JS/vbackplay404.js
@@ -1,11 +1,9 @@
 new BackgroundVideo({
     video: [
-        {file: "https://robocop79.github.io/video/5minrsod.mp4"},
-        {file: "https://robocop79.github.io/video/rsodloopbetter.mp4"},
-        {file: "https://robocop79.github.io/video/rsodloopbetter.mp4"},
-        {file: "https://robocop79.github.io/video/rsodloopbetter.mp4"},
-        {file: "https://robocop79.github.io/video/rsodloopbetter.mp4"},
-        {file: "https://robocop79.github.io/video/rsodloopbetter.mp4"}
+        {file: "https://robocop79.github.io/video/2min30rsod.mp4"},
+        {file: "https://robocop79.github.io/video/rsodloopbest.mp4"},
+        {file: "https://robocop79.github.io/video/rsodloopbest.mp4"},
+        {file: "https://robocop79.github.io/video/rsodloopbest.mp4"}
     ],
     mobileImg: "https://robocop79.github.io/images/Subscribe.png"
     
