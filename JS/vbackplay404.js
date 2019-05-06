5f80b5c13b221d11e1c13fbea356f6e5cfdbaedb
Robocop79 <33665206+Robocop79@users.noreply.github.com>
2019-05-06 08:08:14 -0500

    Better loops

--- JS/vbackplay404.js
+++ JS/vbackplay404.js
@@ -1,11 +1,11 @@
 new BackgroundVideo({
     video: [
-        {file: "https://robocop79.github.io/video/ps2rsodtrim.mp4"},
-        {file: "https://robocop79.github.io/video/ps2rsodloop.mp4"},
-        {file: "https://robocop79.github.io/video/ps2rsodloop.mp4"},
-        {file: "https://robocop79.github.io/video/ps2rsodloop.mp4"},
-        {file: "https://robocop79.github.io/video/ps2rsodloop.mp4"},
-        {file: "https://robocop79.github.io/video/ps2rsodloop.mp4"}
+        {file: "https://robocop79.github.io/video/9min25rsod.mp4"},
+        {file: "https://robocop79.github.io/video/rsodloopgood.mp4"},
+        {file: "https://robocop79.github.io/video/rsodloopgood.mp4"},
+        {file: "https://robocop79.github.io/video/rsodloopgood.mp4"},
+        {file: "https://robocop79.github.io/video/rsodloopgood.mp4"},
+        {file: "https://robocop79.github.io/video/rsodloopgood.mp4"}
     ],
     mobileImg: "https://robocop79.github.io/images/Subscribe.png"
 });
